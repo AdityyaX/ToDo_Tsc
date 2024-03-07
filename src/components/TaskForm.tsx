dbd68c6f879eff36e11466f989844c77e62d215e
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2024-03-07 14:28:35 +0530

    responsiveness fixed

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -4,25 +4,19 @@ import { FormControl } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import AddIcon from '@mui/icons-material/Add';
 
-// export {};
 import React, { useState } from "react";
-export const Muitext = () => {
-    const [todo,setodo] = useState<string>('')
-    const [description,setdescription] = useState<string>('')
-    const [priority,setPriority] = useState<string>('')
-    const [task,settask] = useState<any[]>([])
 
-    // const AddTask = (newtask: any[]) => {
-    //     const updated = [...task, newtask];
-    //     settask(updated);
-    //   };
-    const handledelete = (event: React.MouseEvent<HTMLElement>,id:any) => {
-event.preventDefault();
-settask(task => task.filter(task => task.id !== id));
-// const updatedTasks = [...task]
-// updatedTasks.splice(id,1);
-// settask(updatedTasks);
+export const Muitext = () => {
+    const [todo, setodo] = useState<string>('');
+    const [description, setdescription] = useState<string>('');
+    const [priority, setPriority] = useState<string>('');
+    const [task, settask] = useState<any[]>([]);
+
+    const handledelete = (event: React.MouseEvent<HTMLElement>, id: any) => {
+        event.preventDefault();
+        settask(task => task.filter(task => task.id !== id));
     }
+
     const handleSubmit = (event: React.MouseEvent<HTMLElement>) => {
         event.preventDefault();
         if (!todo || !priority || !description) {
@@ -36,104 +30,91 @@ settask(task => task.filter(task => task.id !== id));
             description: description,
             priority: priority,
         };
-        console.log(newtask);
-        // AddTask([newtask]
-        // ); 
+
         const updated = [...task, newtask];
-        // console.log(updated);
         settask(updated);
-      
     }
-
-
 
     const handleFormatChange = (newAlignment: string) => {
         if (newAlignment !== null) {
             setPriority(newAlignment);
         }
-        console.log(priority);
-        
     }
 
     return (
         <>
-        <Grid
-            container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-            sx={{ minHeight: '100vh', backgroundColor: 'lightblue' }}
-        >
-            <Grid item xs={6}>
-            <Stack direction='column' sx={{ minHeight: '50vh', minWidth:'80vh', marginTop:'20vh', padding: '2vh', borderRadius: '1vh', backgroundColor: 'white' }}>
-                <FormControl>
-                    <Typography variant="h4" gutterBottom component="div" sx={{ textAlign: 'center', color: 'primary.main' }}>
-                        To Do List
-                    </Typography>
-
-                    <Stack direction='column' spacing={2}>
-                        <TextField 
-                            label="title" 
-                            variant="standard"
-
-                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                                setodo(event.target.value);
-                            }}
-                        />
-                        <TextField 
-                            label="Description" 
-                            variant="standard"
-                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                                setdescription(event.target.value);
-                            }} 
-                        />
-
-                        <ToggleButtonGroup  
-                            sx={{ marginTop:'5vh', display:'flex', justifyContent:'center' }} 
-                            onChange={(event, newAlignment) => handleFormatChange(newAlignment)}
-                        >
-                            <ToggleButton value="Highest" color="primary" sx={{ color: 'black' }}>Highest</ToggleButton>
-                            <ToggleButton value="Medium" color="primary" sx={{ color: 'black' }}>Medium</ToggleButton>
-                        </ToggleButtonGroup>
-
-                        <Button 
-                            variant="contained"
-                            color="primary"
-                            onClick={handleSubmit}
-                        >
-               Add Task
-                        </Button>
-                    </Stack>
-                </FormControl>
-               
-            </Stack>
-            </Grid>
-            <Grid item xs={6}>
-                <Stack direction='column' sx={{ minHeight: '50vh', minWidth:'80vh', marginTop:'20vh', padding: '2vh', borderRadius: '1vh', backgroundColor: 'white' }} spacing={2}>
-                <Typography variant="h4" gutterBottom component="div" sx={{ textAlign: 'center', color: 'primary.main' }}>List</Typography>
-                <Stack direction="row" spacing={4}>
-                    <Typography variant="h6">S.NO</Typography>
-                    <Typography variant="h6">Title</Typography>
-                    <Typography variant="h6">Description</Typography>
-                    <Typography variant="h6">Priority</Typography>
-                </Stack>
+            <Grid container spacing={2} sx={{
+        minHeight: '100vh',
+        backgroundColor: 'lightblue',
+        flexDirection: { xs: 'column', sm: 'row' },
         
-        {task.map((task) => (
-            <div key={task.title} >
-                <Stack direction="row" spacing={4}>
-                    <Typography variant="h6">*</Typography>
-                    <Typography variant="h6">{task.title}</Typography>
-                    <Typography variant="h6">{task.description}</Typography>
-                    <Typography variant="h6">{task.priority}</Typography>
-                    <Button onClick={(event: React.MouseEvent<HTMLElement>) => { handledelete(event, task.id) }} variant="contained">Completed</Button>
-                </Stack>
-                
-            </div>
-        ))}
+      }}>
+                <Grid item xs={12} sm={6}>
+                    <Stack direction='column'  sx={{ minHeight: '50vh', padding: '2vh', borderRadius: '1vh', backgroundColor: 'white',marginTop: '20vh', }}>
+                        <FormControl>
+                            <Typography variant="h4" gutterBottom component="div" sx={{ textAlign: 'center', color: 'primary.main' }}>
+                                To Do List
+                            </Typography>
 
-        </Stack>
-<AddIcon/>
+                            <Stack direction='column' spacing={2}>
+                                <TextField
+                                    label="title"
+                                    variant="standard"
+                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                        setodo(event.target.value);
+                                    }}
+                                />
+                                <TextField
+                                    label="Description"
+                                    variant="standard"
+                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                        setdescription(event.target.value);
+                                    }}
+                                />
 
-        </Grid>
-        </Grid>
-       
-       </>
+                                <ToggleButtonGroup
+                                    sx={{ marginTop: '5vh', display: 'flex', justifyContent: 'center' }}
+                                    onChange={(event, newAlignment) => handleFormatChange(newAlignment)}
+                                >
+                                    <ToggleButton value="Highest" color="primary" sx={{ color: 'black' }}>Highest</ToggleButton>
+                                    <ToggleButton value="Medium" color="primary" sx={{ color: 'black' }}>Medium</ToggleButton>
+                                </ToggleButtonGroup>
+
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={handleSubmit}
+                                >
+                                    Add Task
+                                </Button>
+                            </Stack>
+                        </FormControl>
+                    </Stack>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Stack direction='column' sx={{ minHeight: '50vh', padding: '2vh', borderRadius: '1vh', backgroundColor: 'white' ,marginTop: '20vh',}} spacing={2}>
+                        <Typography variant="h4" gutterBottom component="div" sx={{ textAlign: 'center', color: 'primary.main' }}>List</Typography>
+                        <Stack direction="row" spacing={4}>
+                            <Typography variant="h6">S.NO</Typography>
+                            <Typography variant="h6">Title</Typography>
+                            <Typography variant="h6">Description</Typography>
+                            <Typography variant="h6">Priority</Typography>
+                        </Stack>
+
+                        {task.map((task) => (
+                            <div key={task.title} >
+                                <Stack direction="row" spacing={4}>
+                                    <Typography variant="h6">*</Typography>
+                                    <Typography variant="h6">{task.title}</Typography>
+                                    <Typography variant="h6">{task.description}</Typography>
+                                    <Typography variant="h6">{task.priority}</Typography>
+                                    <Button onClick={(event: React.MouseEvent<HTMLElement>) => { handledelete(event, task.id) }} variant="contained">Completed</Button>
+                                </Stack>
+                            </div>
+                        ))}
+                    </Stack>
+                </Grid>
+            </Grid>
+        </>
     );
 }
